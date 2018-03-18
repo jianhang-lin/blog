d62baf943ce5f8ada35f506179333fc44c2a4c92
jianhang-lin <ej2741@email.vccs.edu>
2018-03-18 10:12:08 +0800

    增加UserService测试类

---
+++ src/test/java/work/jianhanglin/spring/boot/blog/service/UserServiceTest.java
@@ -0,0 +1,39 @@
+package work.jianhanglin.spring.boot.blog.service;
+
+import org.junit.Test;
+
+public class UserServiceTest {
+
+	@Test
+	public void testSaveUser() {
+	}
+
+	@Test
+	public void testRemoveUser() {
+	}
+
+	@Test
+	public void testRemoveUsersInBatch() {
+	}
+
+	@Test
+	public void testUpdateUser() {
+	}
+
+	@Test
+	public void testGetUserById() {
+	}
+
+	@Test
+	public void testListUsers() {
+	}
+
+	@Test
+	public void testListUsersByNameLike() {
+	}
+
+	@Test
+	public void testListUsersByUsernames() {
+	}
+
+}
