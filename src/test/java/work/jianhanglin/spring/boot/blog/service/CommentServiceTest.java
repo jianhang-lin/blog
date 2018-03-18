cc39fe5a5a9d918b110157350d85d44699a25618
jianhang-lin <ej2741@email.vccs.edu>
2018-03-18 16:44:49 +0800

    增加commentService的getCommentById的测试代码

--- src/test/java/work/jianhanglin/spring/boot/blog/service/CommentServiceTest.java
+++ src/test/java/work/jianhanglin/spring/boot/blog/service/CommentServiceTest.java
@@ -1,11 +1,15 @@
 package work.jianhanglin.spring.boot.blog.service;
 
 import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import work.jianhanglin.spring.boot.blog.domain.Comment;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +20,8 @@ public class CommentServiceTest {
 
 	@Test
 	public void testGetCommentById() {
+		Comment comment = commentService.getCommentById(1L);
+		Assert.assertThat(comment.getContent(), is("Hello"));
 	}
 
 	@Test
