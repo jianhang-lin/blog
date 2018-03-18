f46d40a7851dd8ebbd727e41cba1e2590bae59fd
jianhang-lin <ej2741@email.vccs.edu>
2018-03-18 12:10:33 +0800

    增加对catalogService的listCatalogs方法的测试代码

--- src/test/java/work/jianhanglin/spring/boot/blog/service/CatalogServiceTest.java
+++ src/test/java/work/jianhanglin/spring/boot/blog/service/CatalogServiceTest.java
@@ -1,29 +1,46 @@
 package work.jianhanglin.spring.boot.blog.service;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import work.jianhanglin.spring.boot.blog.domain.Catalog;
+import work.jianhanglin.spring.boot.blog.domain.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CatalogServiceTest {
+
+	@Autowired
+	private CatalogService catalogService;
+
+	@Autowired
+	private UserService userService;
 
 	@Test
 	public void testSaveCatalog() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveCatalog() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetCatalogById() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testListCatalogs() {
-		fail("Not yet implemented");
+		User user = userService.getUserById(1L);
+		List<Catalog> catalogs = catalogService.listCatalogs(user);
+		Assert.assertThat(catalogs.get(0).getId(), is(1L));
 	}
 
 }
