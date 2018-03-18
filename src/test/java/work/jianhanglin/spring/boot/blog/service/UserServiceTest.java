03a36e40bda1dab8b769f4f45708657052152c9d
jianhang-lin <ej2741@email.vccs.edu>
2018-03-18 10:19:35 +0800

    增加对userService的saveUser方法的测试代码

--- src/test/java/work/jianhanglin/spring/boot/blog/service/UserServiceTest.java
+++ src/test/java/work/jianhanglin/spring/boot/blog/service/UserServiceTest.java
@@ -1,11 +1,27 @@
 package work.jianhanglin.spring.boot.blog.service;
 
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import work.jianhanglin.spring.boot.blog.domain.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserServiceTest {
+
+	@Autowired
+	private UserService userService;
 
 	@Test
 	public void testSaveUser() {
+		User user = userService.saveUser(new User("Tom", "11@qq.com", "John", "123456"));
+		Assert.assertThat(user.getName(), is("Tom"));
 	}
 
 	@Test
