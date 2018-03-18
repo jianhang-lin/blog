e73e0caa4df84dc2226a8f1700fbd6b0cbcab995
jianhang-lin <ej2741@email.vccs.edu>
2018-03-18 22:52:41 +0800

    更新对AdminController的listUsers方法的测试代码

--- src/test/java/work/jianhanglin/spring/boot/blog/controller/AdminControllerTest.java
+++ src/test/java/work/jianhanglin/spring/boot/blog/controller/AdminControllerTest.java
@@ -1,14 +1,17 @@
 package work.jianhanglin.spring.boot.blog.controller;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,8 +22,9 @@ public class AdminControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void testListUsers() {
-		fail("Not yet implemented");
+	public void testListUsers() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/admins").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isFound()).andDo(MockMvcResultHandlers.print()).andReturn();
 	}
 
 }
