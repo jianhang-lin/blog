e60b752d27e8496207b5ab920178fea7b330bdc8
jianhang-lin <ej2741@email.vccs.edu>
2018-03-18 12:05:05 +0800

    新建CatalogService测试类

---
+++ src/test/java/work/jianhanglin/spring/boot/blog/service/CatalogServiceTest.java
@@ -0,0 +1,29 @@
+package work.jianhanglin.spring.boot.blog.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CatalogServiceTest {
+
+	@Test
+	public void testSaveCatalog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCatalog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCatalogById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListCatalogs() {
+		fail("Not yet implemented");
+	}
+
+}
